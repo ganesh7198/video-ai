9779a3d42c1d93986694acb6cded964ae4028d60
ganesh7198 <gg7198596@gmail.com>
2026-07-09 20:15:30 +0530

    database connect

--- lib/db.ts
+++ lib/db.ts
@@ -1,32 +1,37 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI=process.env.MONGODB_URI!
+const MONGODB_URI = process.env.MONGODB_URI!;
 
-if(!MONGODB_URI){
-	throw new Error("define mongo uri in .env")
+if (!MONGODB_URI) {
+  throw new Error("Please define MONGODB_URI in your .env file");
 }
 
+let cached = global.mongoose;
 
-let cached= global.mongoose
-
-if(!cached){
-  cached=global.mongoose ={
-		conn :null,
-		promise:null
-	}
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
-export async function connectToDataBase(){
-	if(cached.conn){
-		return cached.conn
-	}
-	if(!cached.promise){
-		mongoose.connect(MONGODB_URI).then(()=>mongoose.connection)
-	}
 
-	try{
-	cached.conn=await cached.promise
-	}catch(error){
-        console.log('error in the DB connection ');
-	}
-	return cached.conn
+export async function connectToDataBase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI)
+      .then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+
+  return cached.conn;
 }
